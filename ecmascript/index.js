300e75ee0c1f0df6d69c83bf3b02a8db7ee7f3a1
Víctor M. Reyes <vm.reyesal@gmail.com>
2022-07-07 19:52:14 -0600

    docs: ecmascript

---
+++ ecmascript/index.js
@@ -0,0 +1,103 @@
+console.log("Hola mundo!");
+
+// function nameConcat(firstName, lastName) {
+//   var firstName = firstName || 'Ingrese nombre'
+//   var lastName = lastName || 'Ingrese apellido'
+//   return firstName +' '+lastName
+// }
+
+// console.log(nameConcat('Victor'))
+
+// function nameConcat(
+//   firstName = "Ingrese nombre",
+//   lastName = "Ingrese apellido"
+// ) {
+//   return firstName + " " + lastName;
+// }
+
+// console.log(nameConcat("Victor"));
+
+// function nameConcat(firstName, lastName) {
+//   // return firstName + " " + lastName;
+//   return `Su nombre es: ${firstName} ${lastName}`;
+// }
+
+// console.log(nameConcat("Victor", "Reyes"));
+
+// console.log("Mi nombre: Victor\nMi apellido: Reyes")
+
+// console.log(`Mi nombre: Victor
+// Mi apellido: Reyes`)
+
+// const paraph = `
+// <div>
+//   <p>Mi texto</p>
+// </div>
+// `
+// console.log(paraph)
+
+// const lista = [1, 2, 3, 4, 5];
+// console.log(lista[0])
+
+// console.log(objeto.name);
+// console.log(objeto.level);
+// console.log(objeto.breed);
+// console.log(objeto.planet);
+// console.log(objeto["breed"]);
+
+// const { name, level, breed, planet } = objeto;
+
+// console.log(name, breed);
+
+// const objeto = {
+//   name: "Vegeta",
+//   level: 8000,
+//   breed: "saiyajin",
+//   planet: "Vegeta",
+// };
+
+// const characterDBZ = {
+//   ...objeto,
+//   season: [1, 2, 3, 4]
+// }
+
+// console.log(characterDBZ)
+
+// const letras = ["a", "b", "c", "d"];
+// const numeros = [1, 2, 3, 4, 5];
+
+// console.log([...letras, ...numeros])
+
+// let nombre = 'Víctor'
+// nombre = 'Manuel'
+
+// console.log(nombre)
+
+// const nombre = 'Víctor'
+// nombre = 'Manuel'
+
+// console.log(nombre)
+
+// const nombre = {name: 'Victor'}
+// nombre.name = "Manuel"
+
+// console.log(nombre)
+
+// // CamelCase
+// primerNombre
+// // PascalCase
+// PrimerNombre
+// // Snake_Case
+// primer_nombre
+// //
+// PRIMERNOMBRE
+
+// const nameConcat = (firstName, lastName) => {
+//   return `Su nombre es: ${firstName} ${lastName}`;
+// };
+
+// const nameConcat = firstName => {
+//   return `Su nombre es: ${firstName}`;
+// };
+
+// const nameConcat = (firstName) => `Su nombre es: ${firstName}`;
