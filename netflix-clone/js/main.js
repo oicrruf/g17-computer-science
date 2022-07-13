04f3ce8d9ec5aa107d439f97f11f83601670941f
Víctor M. Reyes <vm.reyesal@gmail.com>
2022-07-12 20:21:52 -0600

    feat: render categories

---
+++ netflix-clone/js/main.js
@@ -0,0 +1,2 @@
+const URL = "https://api.themoviedb.org/3";
+const API_KEY = "c93f3323912a37d0bf800fb8f862ac62";
---
+++ netflix-clone/js/select.js
@@ -0,0 +1,19 @@
+const renderSelect = (data) => {
+  let select = document.querySelector("#categories");
+  let { genres } = data;
+
+  genres.forEach((gender) => {
+    // Creamos el nuevo elemento <option>
+    let option = document.createElement("option");
+    // Agregamos el atributo "value"
+    option.setAttribute("value", gender.id);
+    // Agregamos el texto que corresponde
+    option.innerText = gender.name;
+    // Insertamos el <option> generado dentro del elemento <select>
+    select.appendChild(option);
+  });
+};
+
+fetch(`${URL}/genre/movie/list?api_key=${API_KEY}`)
+  .then((response) => response.json())
+  .then((data) => renderSelect(data));
