69f9282c8a76f6b99d1444ce94bf809865afd9f5
Víctor M. Reyes <vm.reyesal@gmail.com>
2022-07-14 20:20:26 -0600

    feat: search movie

--- netflix-clone/js/search.js
+++ netflix-clone/js/search.js
@@ -1,7 +1,38 @@
-let busqueda = document.querySelector("search").value;
+const frmBuscar = document.querySelector("#search");
+const content = document.querySelector("#content");
 
-movies_array.filter((movie)=>{
-    let titulo = movie.title.toLowerCase();
-    let b = busqueda.toLowerCase();
-    return titulo.includes(b);
-})
+const renderResults = (results) => {
+  content.innerHTML = "";
+
+  let block = document.createElement("div");
+  block.classList.add("row", "col-12", "m-0");
+  block.setAttribute("id", "categories-group");
+
+  content.appendChild(block);
+
+  results.forEach((movie) => {
+    console.log(movie);
+    block.insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="col-3 my-3">
+      <img src="${IMG_PREFIX}${movie.poster_path}" width="100%" />
+    </div>
+    `
+    );
+  });
+};
+
+frmBuscar.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const { movieName } = event.target;
+
+  fetch(`${URL}/search/multi?api_key=${API_KEY}&query=${movieName.value}`)
+    .then((response) => response.json())
+    .then((data) => renderResults(data.results));
+});
+
+// let peticion = ['100 años de soledad', 'Spiderman', 'Thor', 'Mi villano favorito', 'Dr. Strange']
+// let result = peticion.filter(name=> name.toLocaleLowerCase().includes(movieName.value.toLocaleLowerCase()))
+// console.log(result)
