1b43e5769dede01ea6293ab93999693ac4a0f7da
Víctor M. Reyes <vm.reyesal@gmail.com>
2022-07-19 20:26:35 -0600

    feat: add favorite

--- netflix-clone/js/popular.js
+++ netflix-clone/js/popular.js
@@ -1,44 +1,52 @@
-let movies_array=[];
+let movies_array = [];
+let allFavorites = [];
+
+const addFavorite = (element) => {
+  let id = element.currentTarget.getAttribute("id");
+  allFavorites.push(id);
+  localStorage.setItem("@favorites", JSON.stringify(allFavorites));
+  element.target.classList.add("border", "border-danger", "border-5");
+};
 
 const renderMovies = (movies) => {
-  document.querySelector("#popular").innerHTML="";
-    //let movies = data.results;
-    movies.forEach((movie) => {
-      let poster = `${IMG_PREFIX}${movie.poster_path}`;
-      //let img_html= `<img src="${poster}" alt="${movie.title}">`
+  document.querySelector("#popular").innerHTML = "";
+  //let movies = data.results;
+  movies.forEach((movie) => {
+    let poster = `${IMG_PREFIX}${movie.poster_path}`;
+    //let img_html= `<img src="${poster}" alt="${movie.title}">`
 
-      let div = document.createElement("div");
-      div.classList.add("col-3","mb-4");
+    let div = document.createElement("div");
+    div.classList.add("col-3", "mb-4");
+    div.setAttribute("id", movie.id);
 
-      let img = document.createElement("img");
-      //img.src=poster;
-      img.classList.add("img-fluid");
-      img.setAttribute("src",poster);
-      img.setAttribute("alt",movie.title)
+    div.addEventListener("click", (e) => addFavorite(e));
 
-      div.append(img)
+    let img = document.createElement("img");
+    //img.src=poster;
+    img.classList.add("img-fluid");
+    img.setAttribute("src", poster);
+    img.setAttribute("alt", movie.title);
 
-      document.querySelector("#popular").append(div);
-      //console.log(movie);
-    })
-  
-  }
+    div.append(img);
+
+    document.querySelector("#popular").append(div);
+    //console.log(movie);
+  });
+};
 
 //function sortByName(){
 const sortByName = () => {
-  movies_array.sort(function(movie1, movie2){
-    return movie1.title > movie2.title ? 1: -1;
+  movies_array.sort(function (movie1, movie2) {
+    return movie1.title > movie2.title ? 1 : -1;
   });
-}
-
+};
 
 //fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
 fetch(`${URL}/movie/popular?api_key=${API_KEY}`)
   .then((response) => response.json())
-  .then((data)=>{
+  .then((data) => {
     movies_array = data.results;
     sortByName();
     /*movies_original = data.results;*/
     renderMovies(data.results);
-});
-
+  });
